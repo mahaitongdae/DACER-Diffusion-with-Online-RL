70b672bea198b62e9081ba82e087463e959a333b
yinuo.wang <THU_wangyinuo@163.com>
2024-12-18 17:46:53 -0900

    [yinuo/feats] add DSACT algorithm

--- scripts/train_mujoco.py
+++ scripts/train_mujoco.py
@@ -6,11 +6,13 @@ from functools import partial
 import jax, jax.numpy as jnp
 
 from relax.algorithm.sac import SAC
+from relax.algorithm.dsact import DSACT
 from relax.algorithm.dacer import DACER
 from relax.algorithm.qsm import QSM
 from relax.algorithm.dipo import DIPO
 from relax.buffer import TreeBuffer
 from relax.network.sac import create_sac_net
+from relax.network.dsact import create_dsact_net
 from relax.network.dacer import create_dacer_net
 from relax.network.qsm import create_qsm_net
 from relax.network.dipo import create_dipo_net
@@ -63,6 +65,9 @@ if __name__ == "__main__":
     elif args.alg == "sac":
         agent, params = create_sac_net(init_network_key, obs_dim, act_dim, hidden_sizes, gelu)
         algorithm = SAC(agent, params, lr=args.lr)
+    elif args.alg == "dsact":
+        agent, params = create_dsact_net(init_network_key, obs_dim, act_dim, hidden_sizes, gelu)
+        algorithm = DSACT(agent, params, lr=args.lr)
     elif args.alg == "dacer":
         def mish(x: jax.Array):
             return x * jnp.tanh(jax.nn.softplus(x))
