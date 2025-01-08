2ea25f4fec5aadb9fae337f86379aad9f59f17c9
haitong_fasrc <haitong@login.rc.fas.harvard.edu>
2025-01-08 11:37:53 -0500

    minor updates on inspect results

--- scripts/inspect_results.py
+++ scripts/inspect_results.py
@@ -11,9 +11,9 @@ import seaborn as sns
 
 sns.set_style('whitegrid')
 
-def plot_mean(patterns_dict: Dict):
+def plot_mean(patterns_dict: Dict, env_name, fig_name = None):
     package_path = Path(relax.__file__)
-    logdir = package_path.parent.parent / 'logs' / 'HalfCheetah-v4'
+    logdir = package_path.parent.parent / 'logs' / env_name
     dfs = []
     for alg, pattern in patterns_dict.items():
         matching_dir = [s for s in logdir.iterdir() if re.match(pattern, str(s))]
@@ -26,13 +26,16 @@ def plot_mean(patterns_dict: Dict):
 
     total_df = pd.concat(dfs, ignore_index=True)
     sns.lineplot(data=total_df, x='step', y='avg_ret', hue='alg')
-    plt.savefig('plot.png')
+    if fig_name is not None:
+        plt.savefig(str)
+    else:
+        plt.show()
     
 
 
-def load_best_results(pattern):
+def load_best_results(pattern, env_name):
     package_path = Path(relax.__file__)
-    logdir = package_path.parent.parent / 'logs' / 'HalfCheetah-v4'
+    logdir = package_path.parent.parent / 'logs' / env_name
     # pattern = r".*diffv2.*noise_scale_0\.0\d$"
     # pattern = r".*diffv2.*noise_scale_0\.09"
     # pattern = r".*qsm.*01-07.*qsm_lr_schedule$"
@@ -53,9 +56,15 @@ def load_best_results(pattern):
 if __name__ == "__main__":
     # pattern = r".*diffv2.*01-07.*diffv2_ema$"
     # load_best_results(pattern)
-    patterns_dict = {'ema': r".*diffv2.*01-07.*diffv2_ema$",
-                     'sampling_ema': r".*diffv2.*01-07.*diffv2_sampling_with_ema$",
-                     'lr_schedule': r".*diffv2.*01-07.*diffv2_lr_schedule$", 
-                     'qsm_lr': r".*qsm.*01-07.*qsm_lr_schedule$",
-                     'qsm': r".*qsm.*01-07.*atp1$"}
-    plot_mean(patterns_dict)
+    # patterns_dict = {
+    #                 #  'ema': r".*diffv2.*01-07.*diffv2_ema$",
+    #                  'sampling_ema': r".*diffv2.*01-07.*diffv2_sampling_with_ema$",
+    #                 #  'lr_schedule': r".*diffv2.*01-07.*diffv2_lr_schedule$", 
+    #                 #  'qsm_lr': r".*qsm.*01-07.*qsm_lr_schedule$",
+    #                  'qsm': r".*qsm.*01-07.*atp1$"}
+    patterns_dict = {
+        'sampling_ema': r".*diffv2.*01.*diffv2_sampling_with_ema$",
+        # 'qsm': r".*qsm.*01.*atp1$",
+        # 'sac': r".*sac.*01.*atp1$"
+    }
+    plot_mean(patterns_dict, 'Ant-v4')
